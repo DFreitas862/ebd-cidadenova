ea22f2ba17a15490497fe48f54e9b6aca7c2219a
DFreitas862 <diegofreitaslw35@gmail.com>
2026-09-13 15:30:20 -0300

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
    CONFIGURAÇÃO DO SUPABASE
    ========================================================= */
 
-const SUPABASE_URL = 'https://aemdbcuoqozmrrtzacfs.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_TZxJfu5BHNVz_DK_pZUbMw_E6N9ngT5';
+const SUPABASE_URL = "https://aemdbcuoqozmrrtzacfs.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_TZxJfu5BHNVz_DK_pZUbMw_E6N9ngT5";
